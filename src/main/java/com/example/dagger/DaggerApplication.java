ff16210e067da95bc93bf6d192287ebb9eb30755
Joe Zhang <sevenkplus@126.com>
2022-02-09 19:48:18 +0800

    init

---
+++ src/main/java/com/example/dagger/DaggerApplication.java
@@ -0,0 +1,13 @@
+package com.example.dagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaggerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dagger/DaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
